ed07a09eca976d73d278d893a6257c5d50323112
黃志仁 <tbsthitw@gmail.com>
2018-12-05 21:31:26 +0800

    add a lot of things

--- js/main.js
+++ js/main.js
@@ -14,35 +14,38 @@ setStyle(blackDiv, {
 });
 document.body.appendChild(blackDiv);
 
-
 // right click menu
 let menu = document.createElement('div');
 menu.id = "right-click-menu";
 menu.innerHTML =
-    `<p>動畫瘋 Plus 選單</p>
-    <div id="off-light" class="item">開關燈</div>
-    <div class="multi-option">
-        <div>
-            跳 OP 
-        </div>
-        <div class="option-container">
-            <button class="op-skipper" data-time="1.5">+1:30</button>
-            |
-            <button class="op-skipper" data-time="0.5">+0:30</button>
-            <button class="op-skipper" data-time="1.0">+1:00</button>
-            <button class="op-skipper" data-time="2.0">+2:00</button>
-            <button class="op-skipper" data-time="2.5">+2:30</button>
-        </div>
+    `<p style="font-weight:bold;">動畫瘋 Plus 選單</p>
+<div id="off-light" class="item">開關燈</div>
+<div id="PiP" class="item">切換子母畫面</div>
+<div class="multi-option">
+    <div>
+        跳 OP 
     </div>
-    <div class="multi-option">
-        <div>
-            播放速度 
-        </div>
-        <div class="option-container">
-            <input type="range" min="0.1" max="2" value="${speed}" step="0.1" class="slider" id="speed-slider">
-            <span id="speed-val">${speed}</span>
-        </div>
-    </div>`;
+    <div class="option-container">
+        <button class="op-skipper" data-time="1.5">+1:30</button>
+        |
+        <button class="op-skipper" data-time="0.5">+0:30</button>
+        <button class="op-skipper" data-time="1.0">+1:00</button>
+        <button class="op-skipper" data-time="2.0">+2:00</button>
+        <button class="op-skipper" data-time="2.5">+2:30</button>
+    </div>
+</div>
+<div class="multi-option">
+    <div>
+        播放速度 
+    </div>
+    <div class="option-container">
+        <input type="range" min="0.5" max="2" value="${speed}" step="0.1" class="slider" id="speed-slider">
+        <span id="speed-val">${speed}</span>
+
+        <button id="reset-speed">Reset</button>
+    </div>
+</div>`;
+
 setStyle(menu, {
     top: 0,
     left: 0,
@@ -52,15 +55,14 @@ setStyle(menu, {
 document.addEventListener('contextmenu', function (e) {
     let appended = !!document.getElementById("right-click-menu");
     if (e.target && e.target.id === 'ani_video_html5_api') {
-        !appended && document.querySelector('#ani_video').appendChild(menu);
+        let video = document.querySelector('#ani_video_html5_api');
+        !appended && document.body.appendChild(menu);
 
         // set context menu position
-        let video = document.querySelector('#ani_video_html5_api');
-        let videoPos = video.getBoundingClientRect();
         setStyle(menu, {
             display: 'block',
-            top: (e.clientY - videoPos.y) + "px",
-            left: (e.clientX - videoPos.x) + "px"
+            top: (e.clientY) + "px",
+            left: (e.clientX) + "px"
         });
 
         // set speed 
@@ -70,27 +72,48 @@ document.addEventListener('contextmenu', function (e) {
             video.playbackRate = speed;
             document.getElementById("speed-val").innerText = speed;
         });
+        let speedReset = document.getElementById("reset-speed");
+        !appended && speedReset.addEventListener('click', (e) => {
+            document.getElementById("speed-val").innerText
+                = video.playbackRate
+                = speed
+                = speedSlider.value
+                = 1;
+        });
 
         // on / off light
         let offLight = document.getElementById("off-light");
         !appended && offLight.addEventListener('click', (e) => {
             isOffLight = !isOffLight;
+            setStyle(document.querySelector('div.video'), {
+                zIndex: isOffLight ? 777 : '',
+            });
             setStyle(blackDiv, {
                 display: isOffLight ? 'block' : 'none'
             })
-            setStyle(
-                document.querySelector('.player'), {
-                    zIndex: isOffLight ? 777 : '',
+        });
+
+        // Picture in picture
+        let PiP = document.getElementById("PiP");
+        !appended && PiP.addEventListener('click', (e) => {
+            if (document.pictureInPictureEnabled) {
+                if (!document.pictureInPictureElement) {
+                    video.requestPictureInPicture()
+                        .catch(error => console.error(error));
+                } else {
+                    document.exitPictureInPicture()
+                        .catch(error => console.error(error));
                 }
-            )
+            } else {
+                alert("你的瀏覽器不支援此功能 QQ");
+            }
         });
 
         // op skipper
         let opSkipper = document.querySelectorAll(".op-skipper");
         !appended && opSkipper.forEach(element => {
             element.addEventListener('click', (e) => {
-                console.log(e.target.dataset.time);
-                video.currentTime += e.target.dataset.time * 60;
+                video.currentTime += e.target.dataset.time * 60 - 3;
             });
         });
 
@@ -102,3 +125,21 @@ window.addEventListener('click', function (e) {
     if (document.getElementById("right-click-menu"))
         setStyle(document.getElementById("right-click-menu"), { display: 'none' })
 })
+
+chrome.storage.sync.get({
+    ncc: true,
+    autoNext: true
+}, function (items) {
+    if (items.ncc) {
+        window.ncc_check = setInterval(function () {
+            if (document.querySelector(".ncc-choose-btn")) {
+                document.getElementById('adult').click();
+                clearInterval(window.ncc_check);
+            }
+        }, 1000);
+    }
+
+    // if (items.autoNext) {
+    //     window.auto_next = 
+    // }
+});
---
+++ popup.js
@@ -0,0 +1,25 @@
+document.getElementById('save')
+    .addEventListener('click', () => {
+        chrome.storage.sync.set({
+            ncc: document.getElementById('ncc').checked,
+            // autoNext: document.getElementById('next').checked
+        }, () => {
+            var status = document.getElementById('status');
+            status.textContent = '設定已儲存';
+            setTimeout(function () {
+                status.textContent = '';
+            }, 750);
+        });
+    });
+
+const restore_options = () => {
+    chrome.storage.sync.get({
+        ncc: true,
+        // autoNext: true
+    }, function (items) {
+        document.getElementById('ncc').checked = items.ncc;
+        // document.getElementById('next').checked = items.autoNext;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
